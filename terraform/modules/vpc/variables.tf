5db00c07ae17d018ade025cfdbeddd48aea06d39
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-07-21 13:34:59 +0700

    - change packer: to ansible-style - change teraform - change ansbile: 2 env + roles - add travis-ci tests from git-repo

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -2,3 +2,11 @@ variable "source_ranges" {
   description = "Allowed IP addresses"
   default     = ["0.0.0.0/0"]
 }
+
+variable "allow_ports" {
+  default = ["22"]
+}
+
+variable "env_name" {
+  default = ""
+}
